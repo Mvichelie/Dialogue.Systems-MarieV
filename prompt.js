617d0abe5aaec22795f10e655176121e466f9c65
Mvichelie <mvichelie@gmail.com>
2024-01-30 14:38:25 +0100

    modified prompt.js

--- prompt.js
+++ prompt.js
@@ -2,7 +2,7 @@ var guess = prompt("Enter a number between 1 to 10 ");
 var randomNumber = Math.floor(Math.random() * 10) + 1;
 
 if (guess != randomNumber){
-    alert("Not matched!" + randomNumber);
+    alert("Not matched! Random number is: " + randomNumber);
 } else {
     alert("Good work!");
 } 
